001045005e4beec3d50705da54de24c4243f2e08
Student <example@example.com>
2019-12-16 20:20:04 -0800

    add some small tweaks to the content of HTML, and also add some js to create values for questions

--- js/scripts.js
+++ js/scripts.js
@@ -8,19 +8,32 @@ $(document).ready(function() {
     $('#under-21').show();
   }
 });
+
 value1 = 1
 value2 = 2
 value3 = 3
-	var range;
-	if (correct < 4) {
-		range = 0;
+
+  var messages = ["You should learn HTML", "You should learn JavaScript", "You should learn Ruby", "You should learn C#"];
+
+  var range;
+  	if ( value <= 3 {
+  		range < 0;
+    if ( value <= 6 {
+    		range < 1;
+    if ( value <= 9 {
+      		range < 3;
+    if ( value <= 15 {
+          range < 3;
+
+
+		range <= 4;
+    range <= 8;
+    range <= 12;
+    range <= 15;
 	}
-	if (correct > 0 && correct < 3) {
-		range = 12;
-	}
-	if (correct > 2) {
-		range = 16;
-	}
+  var messages = ["You should learn JavaScript - It enables interactive web pages and thus is an essential part of web application", "You should learn Ruby - Ruby has built in support for Rational numbers and Complex numbers, which helps in solving complex mathematical problems.", "You should learn C# - C# is good for developing games"];
+
+
 	document.getElementById("after_submit").style.visibility = "visible";
 	document.getElementById("message").innerHTML = messages[range];
 	document.getElementById("number_correct").innerHTML = "You got " + correct + " correct";
